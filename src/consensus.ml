06dac7c9d0b74086234aa73c247be6844ccacd0e
moyo <moyo3ib@gmail.com>
2020-12-09 13:27:33 +0100

    corriger le consensus

--- src/consensus.ml
+++ src/consensus.ml
@@ -1,53 +1,47 @@
 open Word
 open Constants
+open Letter
 
 (* ignoring unused variables - to be removed *)
 let _ = ignore genesis
 
 (* end ignoring unused variables - to be removed *)
 
-let word_score word = 
-  List.fold_right ((letter,acc) -> letter_score letter acc  ) 0 word.word
+let letter_score ~letter : int =  (int_of_char letter.letter) mod 10
 
-let letter_score (letter l _ _ _ _) acc =
-  match l with
-  | 'a' -> acc + 1
-  | 'b' -> acc + 3
-  | 'c' -> acc + 3
-  | 'd' -> acc + 2
-  | 'e' -> acc + 1
-  | 'f' -> acc + 4
-  | 'g' -> acc + 2
-  | 'h' -> acc + 4
-  | 'i' -> acc + 1
-  | 'j' -> acc + 8
-  | 'k' -> acc + 5
-  | 'l' -> acc + 1
-  | 'm' -> acc + 3
-  | 'n' -> acc + 1
-  | 'o' -> acc + 1
-  | 'p' -> acc + 3
-  | 'q' -> acc + 10
-  | 'r' -> acc + 1
-  | 's' -> acc + 1
-  | 't' -> acc + 1 
-  | 'u' -> acc + 1
-  | 'v' -> acc + 4
-  | 'w' -> acc + 4
-  | 'x' -> acc + 8
-  | 'y' -> acc + 4
-  | 'z' -> acc + 10
-    
+let word_score ~word = 
+  List.fold_right 
+    (fun letter score -> (letter_score ~letter) + score) word.word 0
   
-let fitness st word = word_score word
+let fitness st word = 
+  let rec fitness hash score =
+    let word  = Store.get_word st hash in
+      if word.level = 0 then 
+        score 
+      else fitness (word.head) (score + (word_score ~word))
+  in fitness (Word.hash word) 0
 
+let choose word1  word2 st = 
+  let word1,word2 = Option.get word1,Option.get word2 in
+  let fit1,fit2 = fitness st word1,fitness st word2 in
+    if fit1 > fit2 then 
+      Some word1 
+    else if fit1 < fit2 then
+      Some word2 
+    else if word1.signature < word2.signature then 
+      Some word1
+    else if word1.signature > word2.signature then 
+      Some word2
+    else  failwith "fail head"
 
 let head ?level (st : Store.word_store) =
-  (* récupère les mots ayant un level inférieur ou égal au level du paramètre *)
-  let word, _ = List.fold_right ( (w, score) (best_word, best_score) -> if score >= best_score then (w,score) else (best_word, best_score)) (None,0)
-    ((List.map (w -> (w, fitness st w)) (* construction list de tuples (mot, score) *)
-        (List.of_seq (* list *)
-           (Seq.filter (word -> w.level >= level ) (* filtre des mots en fonction de leur level *)
-              (Hashtbl.to_seq_values st.words_table)))) (* table de mots en sequences de mots*)
-    )
-  in word
+  Hashtbl.fold 
+      (fun _ (word:Word.word) w ->
+        if Option.get level != word.level then
+          w 
+        else if w = None then 
+          Some(word) 
+        else 
+          choose (Some word) w st) 
+      (Store.get_words_table st)
+      None
